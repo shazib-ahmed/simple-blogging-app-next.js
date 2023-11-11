596e8e8653e013ac723d4202b7dea45025e54b55
shazib-ahmed <shajibahmed420@gmail.com>
2023-11-11 17:56:00 +0600

    simple blogging app next js

---
+++ src/utils/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBajbbO1KV8R6CbtQq2w0rNOUXMPFeIaGg",
+  authDomain: "nextjs-blog-7eeac.firebaseapp.com",
+  projectId: "nextjs-blog-7eeac",
+  storageBucket: "nextjs-blog-7eeac.appspot.com",
+  messagingSenderId: "198114690345",
+  appId: "1:198114690345:web:a2f73a03999effcbfe6338",
+  measurementId: "G-25ZGZFC0W3",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
